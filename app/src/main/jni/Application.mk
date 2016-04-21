f96957e43609fdbfd37671db566aa323b6e0c170
Bertrand Martel <bmartel.fr@gmail.com>
2016-04-21 13:39:06 +0200

    restrict to sdk 19

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -3,8 +3,8 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 APP_STL := c++_shared
 APP_PLATFORM = android-10
-APP_ABI := armeabi-v7a
-#APP_ABI :=  all
+#APP_ABI := armeabi-v7a
+APP_ABI :=  all
 APP_MODULES := hciviewer
 APP_CPPFLAGS += -frtti 
 APP_CPPFLAGS += -fexceptions
